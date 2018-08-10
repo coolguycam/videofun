9de51f9e2376ad6edf8d71286b209d8ee35db6fd
Cameron Dimitroff <hawtrods@gmail.com>
2018-08-10 10:07:32 -0700

    just to begin

---
+++ server.js
@@ -0,0 +1,20 @@
+// NPM packages used to give server basic functionality
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Creates "express" server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
